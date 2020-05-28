1c9c825b6be04dfccbc0389050ec7c388ba0a2b6
Marius Karnauskas <marius.karnauskas@gmail.com>
2020-05-28 22:27:33 +0000

    Log actions

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.14
 
 require (
 	github.com/artyom/autoflags v1.1.1
+	github.com/davecgh/go-spew v1.1.1
+	github.com/sirupsen/logrus v1.6.0
 	golang.org/x/net v0.0.0-20190227160552-c95aed5357e7
 )
--- webdavserver.go
+++ webdavserver.go
@@ -7,12 +7,16 @@ import (
 	"errors"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 	"time"
 
 	"github.com/artyom/autoflags"
 	"golang.org/x/net/webdav"
+
+	spew "github.com/davecgh/go-spew/spew"
+	logrus "github.com/sirupsen/logrus"
 )
 
 func main() {
@@ -22,22 +26,68 @@ func main() {
 		Auth   string `flag:"auth,basic auth credentials in user:password format (or set WEBDAV_AUTH env)"`
 		TLSKey string `flag:"key,TLS private key for secure connection"`
 		TLSCrt string `flag:"crt,TLS public key for secure conneciton"`
+		Log    bool   `flag:"debug,Log requests"`
 	}{
 		Dir:    ".",
 		Addr:   "127.0.0.1:8080",
 		Auth:   os.Getenv("WEBDAV_AUTH"),
 		TLSKey: "",
 		TLSCrt: "",
+		Log:    false,
 	}
 	autoflags.Parse(&p)
-	log.Fatal(serve(p.Dir, p.Addr, p.Auth, p.TLSCrt, p.TLSKey))
+	log.Fatal(serve(p.Dir, p.Addr, p.Auth, p.TLSCrt, p.TLSKey, p.Log))
 }
 
-func serve(dir, addr, auth, tlscrt, tlskey string) error {
+type logger struct {
+	l *logrus.Logger
+}
+
+func (self *logger) Log(r *http.Request, e error) {
+
+	switch r.Method {
+	case "PROPFIND":
+	case "GET":
+		self.l.Infof("%s: %s %s", r.RemoteAddr, r.Method, r.RequestURI)
+	case "MOVE":
+		u, err := url.Parse(r.Header["Destination"][0])
+		if err != nil {
+			self.l.Errorf("%s: %s %s : %s", r.RemoteAddr, r.Method, r.RequestURI, e)
+			return
+		}
+		self.l.Infof("%s: %s %s => %s", r.RemoteAddr, r.Method, r.RequestURI, u.Path)
+	case "COPY":
+		u, err := url.Parse(r.Header["Destination"][0])
+		if err != nil {
+			self.l.Errorf("%s: %s %s : %s", r.RemoteAddr, r.Method, r.RequestURI, e)
+			return
+		}
+		self.l.Infof("%s: %s %s => %s", r.RemoteAddr, r.Method, r.RequestURI, u.Path)
+	case "DELETE":
+		self.l.Infof("%s: %s %s", r.RemoteAddr, r.Method, r.RequestURI)
+	case "MKCOL":
+		self.l.Infof("%s: %s %s", r.RemoteAddr, "MKDIR", r.RequestURI)
+	case "PUT":
+		self.l.Infof("%s: %s %s %d", r.RemoteAddr, r.Method, r.RequestURI, r.ContentLength)
+	default:
+		spew.Dump(r)
+		self.l.Infof("%s: %s %s", r.RemoteAddr, r.Method, r.RequestURI)
+	}
+
+	if e != nil {
+		self.l.Errorf("%s: %s", r.RemoteAddr, e)
+	}
+}
+
+func serve(dir, addr, auth, tlscrt, tlskey string, log bool) error {
 	var handler http.Handler
 	webdavHandler := &webdav.Handler{
 		FileSystem: webdav.Dir(dir),
 		LockSystem: webdav.NewMemLS(),
+	}
+	if log {
+		l := &logger{l: logrus.New()}
+		webdavHandler.Logger = l.Log
 	}
 	handler = webdavHandler
 	if auth != "" {
